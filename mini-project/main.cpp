97c43f09d90dea612fcc7746d6b8c03030e15f65
Johan Hagman <herr.hagman@gmail.com>
2021-10-29 11:54:36 +0200

    bug fix

--- mini-project/main.cpp
+++ mini-project/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
         solver.PrintGridAsLine();
         // PrintGridState(puzzle);
 
-        if (solver.Search(guesses) == true) {
+        if (solver.Search() == true) {
             std::cout<<"Search with " << solver.guesses << " numbers guessed:"<<std::endl;
             solver.PrintGridAsLine();
             // PrintGrid(puzzle);
